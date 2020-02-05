ec643661e1f1cda519d49922a1ab65ac9fc813be
ECH-C <ECH-C@192.168.109.130>
2020-02-05 13:27:38 +0100

    ths is my first commit of the project javainitiation

---
+++ Javainitiation/src/Variables/Test_Mutex.java
@@ -0,0 +1,40 @@
+package Variables;
+
+public class Test_Mutex 
+{
+
+	//public static void main(String[] args) 		
+		/*
+		//Question 01  : Quelle sera la sortie console après l'éxécution de ce code?
+		Integer a = new Integer (1);
+		Integer b = new Integer (1);
+		if ( a == b )
+		System.out.println("a==b" );
+		else
+		System.out.println("a!=b" );*/
+		
+	/*
+		// Question 02 :  Quelle sera la sortie console après l'éxécution de ce code?
+		{
+		Person p = new Person ("Henry");
+		updatePersonne (p);
+		System.out.println(p.getName());
+		}
+		
+		public static void updatePersonne(Person p) 
+				{ p = new Person ("John"); }
+		
+			private static class Person 
+			
+			{
+				private String name;
+		
+				public Person (String name) 
+					{super();
+					this.name = name;}
+
+				public String getName() {return name;}
+		
+			} */
+
+}
---
+++ Javainitiation/src/Variables/Varaibles_01.java
@@ -0,0 +1,88 @@
+package Variables;
+
+public class Varaibles_01 {
+
+	public static void main(String[] args) {
+		/*String carsName = "BMW";
+		String year_word = "Years old";
+		final int aGeN = 20;
+		int warranty = 10;
+		int age = aGeN + warranty;
+		float consu_gas = 6.415f;
+		char serie = 'S';
+		String full_name_brand = carsName + " " + serie;
+		boolean gasoil = true;*/
+		//System.out.println(full_name_brand + " " + age  + " " + year_word + " " + consu_gas + "L/100KM");
+		
+		
+		/* Division
+		int A = 100, b = 200, C = 300;
+		int Ab = b/A;
+		System.out.println(Ab + C);*/
+		
+		
+		/*double gRand_chiffre = 10.251551514498714568;
+		System.out.println(gRand_chiffre);*/
+		
+
+		/*// double Stores fractionnal numbers. Sufficient for storing 15 decimal digits
+		// in our cas, we are going to tranforme a variable from double to int
+		double gRand_chiffre = 10.251551514498714568d;
+		int gRa_ch = (int) gRand_chiffre;
+		System.out.println(gRand_chiffre);
+		System.out.println(gRa_ch);*/
+		
+		
+		/*//here we are going to see narrowing casting (manually)
+			byte taille_R = 110;
+			int taille_N = (int) taille_R; // manual casting
+			taille_N = 200;
+			System.out.println(taille_R);
+			System.out.println(taille_N);
+		
+		//here we are going to see Widening casting (automatically)
+			// EX 01 :
+			byte taille_E = 115;
+			int taille_A =  taille_E; // automatic casting into in
+			taille_A = 250;
+			System.out.println(taille_E);
+			System.out.println(taille_A);
+		
+		
+			// EX 02 :
+			int taille_F = 120;
+			double taille_D =  taille_E; // automatic casting into in
+			taille_A = 150;
+			System.out.println(taille_F);
+			System.out.println(taille_D);
+			
+		*/
+
+		
+		
+		/*//Pour indiquer le nombre de "0" après le chiffre ( chiffre "e" nombre de "0" ) pour les chiffres à virgule ;
+		float f1 = 35e4f;
+		double d1 = 12e1d;
+		System.out.println(f1);
+		System.out.println(d1);
+		*/
+		
+		/*//boolean values are mostly used for conditional testing 
+		boolean here = true;
+		boolean there = false;
+		System.out.println(here);
+		System.out.println(there); */
+		
+		
+		/*//Increment and Decrement
+		int A = 100, b = 200, C = 90, D = 200;
+		int Ab = b/A;
+		System.out.println(++Ab ); // Increases the value of the variable by 1 so as a result >> "3"
+		System.out.println(--C ); //>> Decrease the value of a variable by 1 so as a result >> "1"
+		System.out.println(D % C );*/
+		
+	
+		
+	}
+	
+}
